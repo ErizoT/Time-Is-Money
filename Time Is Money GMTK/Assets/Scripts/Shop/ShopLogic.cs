6dae45c9005a23cec22a9e8769a41e1295c646df
ErizoT <134489594+ErizoT@users.noreply.github.com>
2026-07-25 16:01:25 +1000

    Working on ShopLogic

---
+++ Time Is Money GMTK/Assets/Scripts/Shop/ShopLogic.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ShopLogic : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Time Based Upgrades
+
+    public void SlowTime(int timeTickRate)
+    {
+        // Subtract the cost from player money
+        // Half the current timeTickRate
+        // Multiply timeSlowCost by 3x
+        
+        
+
+        
+        // timeTickRate /= 2;
+        
+        
+        // timeSlowCost *= 3;
+    }
+
+    public void SpeedLuck()
+    {
+        // Subtract cost from player money
+        // Increase tickrate by .25
+        // Double player luck
+
+
+    }
+}
