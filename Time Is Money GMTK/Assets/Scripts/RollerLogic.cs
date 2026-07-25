6ef9a914efb1074a7dfac7bd00b5826e734be571
James McCallum <86056099+questionabl@users.noreply.github.com>
2026-07-25 13:54:01 +1000

    Added RollerSymbols and GlobalLuck

--- Time Is Money GMTK/Assets/Scripts/RollerLogic.cs
+++ Time Is Money GMTK/Assets/Scripts/RollerLogic.cs
@@ -1,8 +1,7 @@
 using UnityEngine;
 
-public class RollerScript : MonoBehaviour
+public class RollerLogic : MonoBehaviour
 {
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,4 +13,5 @@ public class RollerScript : MonoBehaviour
     {
         
     }
+    
 }
---
+++ Time Is Money GMTK/Assets/Scripts/RollerSymbols.cs
@@ -0,0 +1,12 @@
+public enum RollerSymbols : int
+{
+    Nothing = 0,
+    Coin = 1,
+    Banknote = 2,
+    Halo = 3,
+    Devil = 4,
+    Skull = 5,
+    Clock = 6,
+    Seven = 7,
+    Angel = 8,
+}
