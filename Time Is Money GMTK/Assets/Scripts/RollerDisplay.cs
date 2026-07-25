8068653c4c3b4ef0ff4262d93cbab1053007b1cf
ErizoT <134489594+ErizoT@users.noreply.github.com>
2026-07-25 14:25:12 +1000

    Update RollerDisplay.cs

--- Time Is Money GMTK/Assets/Scripts/RollerDisplay.cs
+++ Time Is Money GMTK/Assets/Scripts/RollerDisplay.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class RollerDisplay : MonoBehaviour
 {
+    //public int value1, value2, value3;
+    public RollerSymbols.Enum rollerSymbol1, rollerSymbol2, rollerSymbol3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +14,11 @@ public class RollerDisplay : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        // Clicking 
+    }
+
+    public void Roll(int roll1, int roll2, int roll3)
     {
         
     }
