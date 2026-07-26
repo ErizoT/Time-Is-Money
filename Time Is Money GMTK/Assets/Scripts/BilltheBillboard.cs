a2d66665bb36e7e97f627dbda4e3bb88779004df
ErizoT <134489594+ErizoT@users.noreply.github.com>
2026-07-26 23:32:36 +1000

    Bill the Billboard

--- Time Is Money GMTK/Assets/Scripts/BilltheBillboard.cs
+++ Time Is Money GMTK/Assets/Scripts/BilltheBillboard.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class BilltheBillboard : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void LateUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        transform.LookAt(Camera.main.transform.position);
     }
 }
